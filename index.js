d1916b58ad38e159aab504364cd054259ec09a6a
Yiğit Akkaya <ygtakky@gmail.com>
2022-02-15 16:38:09 +0300

    [MOD] inde.js for validation

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function Li(list, subscriber) {
       const newLi = document.createElement("li");
       newLi.setAttribute("class", "list-group-item");
       newLi.setAttribute("history-id", element.id);
-      newLi.innerHTML = `<div">${element.sender} just sent ${element.receiver} ${element.amount} &#8378<span style="float: right;">&#10003</span></div>`;
+      newLi.innerHTML = `<div">${element.time.toLocaleTimeString()} - ${element.sender} sent ${element.receiver} ${element.amount} &#8378<span style="float: right;">&#10003</span></div>`;
       subscriber.appendChild(newLi);
     });
   }
@@ -34,7 +34,7 @@ function Li(list, subscriber) {
 function Option(list, subscriber) {
   list.forEach(element => {
     const newOption = document.createElement("option");
-    newOption.setAttribute("value", element.name);
+    newOption.setAttribute("value", element.id);
     newOption.innerText = element.name;
     subscriber.appendChild(newOption);
   })
@@ -76,19 +76,21 @@ function setState(stateName, newValue) {
 // Account adding function
 function addAccount(event) {
   event.preventDefault();
-  const accountName = document.getElementById("accountName").value;
-  const accountBalance = document.getElementById("accountBalance").value;
+  const accountName = document.getElementById("accountName");
+  const accountBalance = document.getElementById("accountBalance");
   if (!(accountName) || !(accountBalance)) {
     return
   }
   setState("accountList", [
     ...state.accountList,
     {
-      id: Math.round(Math.random()*10000),
-      name: accountName,
-      balance: accountBalance
+      id: String(Math.round(Math.random()*10000)),
+      name: accountName.value,
+      balance: accountBalance.value
     }
   ]);
+  accountName.value = "";
+  accountBalance.value = "";
   renderAccounts();
   renderOptions();
 }
@@ -97,32 +99,38 @@ function addAccount(event) {
 function sendMoney() {
   const senderOptions = document.getElementById("senderNames");
   const receiverOptions = document.getElementById("receiverNames");
-  const senderName = senderOptions.options[senderOptions.selectedIndex].value;
-  const receiverName = receiverOptions.options[receiverOptions.selectedIndex].value;
+  const senderId = senderOptions.options[senderOptions.selectedIndex].value;
+  const receiverId = receiverOptions.options[receiverOptions.selectedIndex].value;
   const amount = document.getElementById("transferAmount").value;
-  addHistory(senderName, receiverName, amount)
-  changeBalance(senderName, receiverName, amount)
+  if (senderId === receiverId) {
+    return
+  }
+  addHistory(senderId, receiverId, amount)
+  changeBalance(senderId, receiverId, amount)
 }
 
 // History adding function
-function addHistory(senderName, receiverName, amount) {
+function addHistory(senderId, receiverId, amount) {
+  const senderName = state.accountList.find(account => account.id === senderId)
+  const receiverName = state.accountList.find(account => account.id === receiverId)
   setState("historyList", [
-    ...state.historyList,
     {
-      id: Math.round(Math.random()*10000),
-      sender: senderName,
-      receiver: receiverName,
+      id: String(Math.round(Math.random()*10000)),
+      time: new Date(),
+      sender: senderName.name,
+      receiver: receiverName.name,
       amount: amount
-    }
+    },
+    ...state.historyList
   ])
   renderHistory();
 }
 
 // Account balance changing function
-function changeBalance(senderName, receiverName, amount) {
+function changeBalance(senderId, receiverId, amount) {
   const accountList = state.accountList;
-  const senderIndex = accountList.findIndex(account => account.name === senderName); // Get the index of the sender
-  const receiverIndex = accountList.findIndex(account => account.name === receiverName); // Get the index of the receiver
+  const senderIndex = accountList.findIndex(account => account.id === senderId); // Get the index of the sender
+  const receiverIndex = accountList.findIndex(account => account.id === receiverId); // Get the index of the receiver
   accountList[senderIndex] = {...accountList[senderIndex], balance: String(Number(accountList[senderIndex].balance) - Number(amount))} // Decrease the balance of sender by sent amount
   accountList[receiverIndex] = {...accountList[receiverIndex], balance: String(Number(accountList[receiverIndex].balance) + Number(amount))} // Increase the balance of receiver by the received amount
   setState("accountList", accountList)
