d0a0288b215c453797b883b401c910a4bb37f284
Yiğit Akkaya <ygtakky@gmail.com>
2022-02-18 20:02:53 +0300

    [ADD] new features added (product adding, cart)

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const state = {
   accountList: [],
   historyList: [],
-  filterList: [{ sender: "", receiver: ""}],
+  filterList: [{ sender: "", receiver: "" }],
+  productList: [],
+  cart: [{ currentUser: "",cartTotal: 0 }],
 };
 
 // State changing function
@@ -18,39 +20,74 @@ window.addEventListener("load", () => {
 // List item creation function
 function Li(list, subscriber) {
   // Generate list items depending on the subscriber
-  if (subscriber.id === "accountsList") {
-    list.forEach((element) => {
-      const newLi = document.createElement("li");
-      newLi.setAttribute(
-        "class",
-        "list-group-item d-flex justify-content-between align-items-center"
-      );
-      newLi.setAttribute("account-id", element.id); // Give the list item the unique account id
-      newLi.innerHTML = `<div class="ms-2 me-auto">${element.name}</div><div class="me-4">${element.balance} &#8378</div><button class="btn btn-danger btn-sm" onclick="removeAccount(event)">X</button>`;
-      subscriber.appendChild(newLi);
-    });
-  } else {
-    list.forEach((element) => {
-      const newLi = document.createElement("li");
-      newLi.setAttribute(
-        "class",
-        "list-group-item d-flex justify-content-between align-items-center"
-      );
-      newLi.setAttribute("history-id", element.id); // Give the list item the unique history id
-      newLi.innerHTML = `<div class="ms-2 me-auto">${element.time.toLocaleTimeString()} - ${
-        element.sender
-      } sent ${element.receiver} ${
-        element.amount
-      } &#8378</div><button class="btn btn-danger btn-sm" onclick="cancelTransaction(event)">Cancel</button>`;
-      subscriber.appendChild(newLi);
-    });
+  switch (subscriber.id) {
+    case "accountsList":
+      list.forEach((element) => {
+        const newLi = document.createElement("li");
+        newLi.setAttribute(
+          "class",
+          "list-group-item d-flex justify-content-between align-items-center"
+        );
+        newLi.setAttribute("account-id", element.id); // Give the list item the unique account id
+        newLi.innerHTML = `<div class="ms-2 me-auto">${element.name}</div><div class="me-4">${element.balance} &#8378</div><button class="btn btn-danger btn-sm" onclick="removeAccount(event)">X</button>`;
+        subscriber.appendChild(newLi);
+      });
+      break;
+    case "historyList":
+      list.forEach((element) => {
+        const newLi = document.createElement("li");
+        newLi.setAttribute(
+          "class",
+          "list-group-item d-flex justify-content-between align-items-center"
+        );
+        newLi.setAttribute("history-id", element.id); // Give the list item the unique history id
+        newLi.innerHTML = `<div class="ms-2 me-auto">${element.time.toLocaleTimeString()} - ${
+          element.sender
+        } sent ${element.receiver} ${
+          element.amount
+        } &#8378</div><button class="btn btn-danger btn-sm" onclick="cancelTransaction(event)">Cancel</button>`;
+        subscriber.appendChild(newLi);
+      });
+      break;
+    case "productList":
+      list.forEach((element) => {
+        const newLi = document.createElement("li");
+        newLi.setAttribute(
+          "class",
+          "list-group-item d-flex align-items-center justify-content-between"
+        );
+        newLi.setAttribute("product-id", element.id); // Give the list item the unique history id
+        newLi.innerHTML = `${element.name} - ${element.quantity} in stock - ${element.price} &#8378 <button class="btn btn-sm btn-success" onclick="addToCart(event)">Add to cart</button>`;
+        subscriber.appendChild(newLi);
+      });
+      break;
+    case "cart":
+      list[0].products.forEach((element) => {
+        const newLi = document.createElement("li");
+        newLi.setAttribute(
+          "class",
+          "list-group-item d-flex align-items-center justify-content-between"
+        );
+        newLi.setAttribute("cartProduct-id", element.id); // Give the list item the unique history id
+        newLi.innerHTML = `${element.name} - ${element.quantity} in cart - ${element.price} &#8378`;
+        subscriber.appendChild(newLi);
+      });
+      break;
+    default:
+      break;
   }
 }
+
+// List item creation for product list function
+function ProductLi(list, subscriber) {}
 
 // Option creation function for the selection menu
 function Option(list, subscriber) {
   list.forEach((element) => {
     const newOption = document.createElement("option"); // Create the option element
+    if (state.cart[0].currentUser === element.id) {
+      newOption.setAttribute("selected", true)
+    }
     newOption.setAttribute("value", element.id); // Set option value to the account id
     newOption.innerText = element.name; // Write the account name into the option
     subscriber.appendChild(newOption);
@@ -71,7 +108,8 @@ function renderOptions() {
     document.getElementById("senderNames"),
     document.getElementById("receiverNames"),
     document.getElementById("senderFilter"),
-    document.getElementById("receiverFilter")
+    document.getElementById("receiverFilter"),
+    document.getElementById("cartUser"),
   ];
   // Generate default options and required options for the sender and receiver lists
   optionList.forEach(function (subscriber) {
@@ -88,6 +126,13 @@ function renderOptions() {
       case "receiverFilter":
         subscriber.innerHTML = `<option value="" selected>Receiver</option>`;
         break;
+      case "cartUser":
+        if (state.cart[0].currentUser === "") {
+          subscriber.innerHTML = `<option value="" disabled selected hidden>User</option>`;
+        } else {
+          subscriber.innerHTML = ``
+        }
+        break;
       default:
         break;
     }
@@ -102,9 +147,9 @@ function addFilter() {
   setState("filterList", [
     {
       sender: senderFilter,
-      receiver: receiverFilter
-    }
-  ])
+      receiver: receiverFilter,
+    },
+  ]);
   renderHistory();
 }
 
@@ -112,19 +157,34 @@ function addFilter() {
 function filterList() {
   const filterList = state.filterList[0]; // Get the filter from the state
   const historyList = state.historyList; // Get the history list from the state
-  if (filterList.sender === "" && filterList.receiver === "") { // By default dont filter history
+  if (filterList.sender === "" && filterList.receiver === "") {
+    // By default dont filter history
     return historyList;
-  } else if (filterList.receiver === "") { // If the receiver filter is empty filter by sender filter
-    return historyList.filter(history => history.senderId === filterList.sender);
-  } else if (filterList.sender === "") { // If the sender filter is empty filter by receiver filter
-    return historyList.filter(history => history.receiverId === filterList.receiver);
-  } else if (filterList.sender === filterList.receiver) { // If sender and receiver filter is the same bring the all transactions with that account id
-    return historyList.filter(history => {
-      return history.senderId === filterList.sender || history.receiverId === filterList.receiver
+  } else if (filterList.receiver === "") {
+    // If the receiver filter is empty filter by sender filter
+    return historyList.filter(
+      (history) => history.senderId === filterList.sender
+    );
+  } else if (filterList.sender === "") {
+    // If the sender filter is empty filter by receiver filter
+    return historyList.filter(
+      (history) => history.receiverId === filterList.receiver
+    );
+  } else if (filterList.sender === filterList.receiver) {
+    // If sender and receiver filter is the same bring the all transactions with that account id
+    return historyList.filter((history) => {
+      return (
+        history.senderId === filterList.sender ||
+        history.receiverId === filterList.receiver
+      );
     });
-  } else { // If there is a sender and receiver filter bring all the transactions of the sender to receiver
-    return historyList.filter(history => {
-      return history.senderId === filterList.sender && history.receiverId === filterList.receiver
+  } else {
+    // If there is a sender and receiver filter bring all the transactions of the sender to receiver
+    return historyList.filter((history) => {
+      return (
+        history.senderId === filterList.sender &&
+        history.receiverId === filterList.receiver
+      );
     });
   }
 }
@@ -138,8 +198,7 @@ function renderHistory() {
 }
 
 // Account adding function
-function addAccount(event) {
-  event.preventDefault(); // Prevent page refresh on submit
+function addAccount() {
   const accountName = document.getElementById("accountName"); // Get the name input element
   const accountBalance = document.getElementById("accountBalance"); // Get the balance input element
   // Check if the inputs are correct
@@ -150,14 +209,14 @@ function addAccount(event) {
   setState("accountList", [
     ...state.accountList,
     {
-      id: String(Math.round(Math.random() * 10000)),
+      id: idGen(),
       name: accountName.value,
-      balance: accountBalance.value,
+      balance: Number(accountBalance.value),
     },
   ]);
   // Reset input values
   accountName.value = "";
-  accountBalance.value = "";
+  accountBalance.value = null;
   // Render accounts and select menu options
   renderAccounts();
   renderOptions();
@@ -170,7 +229,15 @@ function removeAccount(event) {
     (account) => account.id !== accountId
   ); // Remove the account from the account list
   setState("accountList", accountList); // Update the state with the new account list
+  if (state.cart[0].currentUser === accountId) {
+    setState("cart", [{
+      currentUser: "",
+      products: [],
+      total: 0
+    }])
+  }
   // Render account list and select menu options
+  renderCart();
   renderAccounts();
   renderOptions();
 }
@@ -190,7 +257,8 @@ function sendMoney() {
   // Add transaction to the history
   addHistory(senderId, receiverId, amount);
   // Do the transaction between the sender and receiver
-  changeBalance(senderId, receiverId, amount);
+  changeBalance(senderId, -amount);
+  changeBalance(receiverId, amount);
 }
 
 // History adding function
@@ -202,13 +270,13 @@ function addHistory(senderId, receiverId, amount) {
   // Add the transaction to the top of the history state
   setState("historyList", [
     {
-      id: String(Math.round(Math.random() * 10000)), // A random unique id
+      id: idGen(), // A random unique id
       time: new Date(), // Transaction time (current time)
       sender: sender.name,
       senderId: sender.id,
       receiverId: receiver.id,
       receiver: receiver.name,
-      amount: amount,
+      amount: Number(amount),
     },
     ...state.historyList,
   ]);
@@ -244,30 +312,165 @@ function cancelTransaction(event) {
     );
     // Cancel the transaction if the accounts still exist
   } else {
-    changeBalance(history.receiverId, history.senderId, history.amount); // Revert the transaction by sending money back
+    // Revert the transaction by sending money back
+    changeBalance(history.receiverId, -history.amount);
+    changeBalance(history.senderId, history.amount);
     deleteHistory(historyId); // Delete the history from the history state and render history list
   }
 }
 
-// Account balance changing function
-function changeBalance(senderId, receiverId, amount) {
+// Account balance change function
+function changeBalance(accountId, amount) {
   const accountList = state.accountList; // Get the account state
-  const senderIndex = accountList.findIndex(
-    (account) => account.id === senderId
-  ); // Get the index of the sender
-  const receiverIndex = accountList.findIndex(
-    (account) => account.id === receiverId
-  ); // Get the index of the receiver
-  accountList[senderIndex] = {
-    ...accountList[senderIndex],
-    balance: String(Number(accountList[senderIndex].balance) - Number(amount)),
-  }; // Decrease the balance of the sender by sent amount
-  accountList[receiverIndex] = {
-    ...accountList[receiverIndex],
-    balance: String(
-      Number(accountList[receiverIndex].balance) + Number(amount)
-    ),
-  }; // Increase the balance of receiver by the received amount
+  const accountIndex = accountList.findIndex(
+    (account) => account.id === accountId
+  ); // Get the index of the account
+  accountList[accountIndex] = {
+    ...accountList[accountIndex],
+    balance: accountList[accountIndex].balance + Number(amount),
+  };
   setState("accountList", accountList); // Set the account state to the new values
   renderAccounts(); // Render account list
+}
+
+// Product stock change function
+function changeStock(cartProductList, sign) {
+  const productList = state.productList;
+  if (sign === "+")
+  {
+    cartProductList.forEach((cartProduct, index) => {
+      const productIndex = productList.findIndex(product => product.id === cartProduct.id);
+      productList[productIndex] = {
+        ...productList[productIndex],
+        quantity: productList[productIndex] + cartProductList[index].quantity
+      };
+    })
+  } else if (sign === "-") {
+    cartProductList.forEach((cartProduct, index) => {
+      const productIndex = productList.findIndex(product => product.id === cartProduct.id);
+      productList[productIndex] = {
+        ...productList[productIndex],
+        quantity: productList[productIndex].quantity - cartProductList[index].quantity
+      };
+    })
+  }
+  setState("productList", productList);
+  renderProducts();
+}
+
+// Product add function
+function addProduct() {
+  const productName = document.getElementById("productName");
+  const productPrice = document.getElementById("productPrice");
+  const productQuantity = document.getElementById("productQuantity");
+  if (productName && productPrice && productQuantity) {
+    setState("productList", [
+      ...state.productList,
+      {
+        id: idGen(),
+        name: productName.value,
+        price: Number(productPrice.value),
+        quantity: Number(productQuantity.value),
+      },
+    ]);
+    // Reset product input values
+    productName.value = "";
+    productPrice.value = null;
+    productQuantity.value = null;
+    renderProducts();
+  } else {
+    alert("Invalid product input!");
+  }
+}
+
+// Render products function
+function renderProducts() {
+  const productList = document.getElementById("productList"); // Get the product list element
+  productList.innerHTML = ""; // Clear the product list element
+  Li(state.productList, productList); // Create list items for each history in the state to the history list
+}
+
+// Set cart user function
+function setCartUser() {
+  const cartUser = document.getElementById("cartUser").value;
+  setState("cart", [
+    {
+      currentUser: cartUser,
+      products: [],
+      total: 0,
+    },
+  ]);
+  renderCart();
+}
+
+// Add cart function
+function addToCart(event) {
+  if (!state.cart[0].currentUser) {
+    alert("No user selected for cart!");
+  } else {
+    const cartProducts = state.cart[0].products;
+    const productId = event.target.parentElement.attributes["product-id"].value;
+    const productIndex = cartProducts.findIndex(
+      (product) => product.id === productId
+    );
+    if (productIndex !== -1) {
+      cartProducts[productIndex] = {
+        ...cartProducts[productIndex],
+        quantity: cartProducts[productIndex].quantity + 1,
+      };
+    } else {
+      const product = state.productList.find(
+        (product) => product.id === productId
+      );
+      cartProducts.push({
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+    const cartTotal = state.cart[0].products.reduce((acc, curr) => {
+      return acc + curr.quantity * curr.price;
+    }, 0);
+    setState("cart", [
+      {
+        currentUser: state.cart[0].currentUser,
+        products: cartProducts,
+        total: cartTotal
+      },
+    ]);
+    renderCart();
+  }
+}
+
+// Checkout function
+function checkout() {
+  const currentAccountId = state.cart[0].currentUser;
+  const total = state.cart[0].total;
+  changeBalance(currentAccountId, -total);
+  changeStock(state.cart[0].products, "-");
+  setState("cart", [
+    {
+      currentUser: currentAccountId,
+      products: [],
+      total: 0
+    }
+  ])
+  renderAccounts();
+  renderCart();
+}
+
+// Render cart function
+function renderCart() {
+  const cart = document.getElementById("cart");
+  const cartTotalElement = document.getElementById("cartTotal");
+  const cartTotal = state.cart[0].total;
+  cartTotalElement.innerHTML = `${cartTotal} &#8378`;
+  cart.innerHTML = "";
+  Li(state.cart, cart);
+}
+
+// Id generator helper function
+function idGen() {
+  return String(Math.round(Math.random() * 10000));
 }
